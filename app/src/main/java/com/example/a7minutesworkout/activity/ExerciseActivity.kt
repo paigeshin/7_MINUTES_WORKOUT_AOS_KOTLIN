90b13f76892d10b0111f0e3dd818458dcb3144bb
paigeshin <paige@paigeshins-MacBook-Pro.local>
2020-12-15 16:10:06 +0900

    completed

--- app/src/main/java/com/example/a7minutesworkout/activity/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/activity/ExerciseActivity.kt
@@ -29,12 +29,12 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     // ** Timer **
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
-    private var restTimerDuration: Long = 1 //10
+    private var restTimerDuration: Long = 10 //10
 
     // ** Exercise Timer **
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
-    private var exerciseTimerDuration: Long = 1 //30
+    private var exerciseTimerDuration: Long = 30 //30
 
     // ** Use Exercise Data **
     private var exerciseList: ArrayList<ExerciseModel>? = null
